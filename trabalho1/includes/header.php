d5e8f8723218c7b07957f60eee4ba96408b619a2
Guilherme-Peixe <54917147+Guilherme-Peixe@users.noreply.github.com>
2019-10-01 10:17:56 -0300

    Add files via upload

---
+++ trabalho1/includes/header.php
@@ -0,0 +1,61 @@
+<!-- HEADER -->
+<header>
+	<!-- top Header -->
+	<div id="top-header">
+		<div class="container">
+			<div class="pull-left">
+				<span>Welcome to E-shop!</span>
+			</div>
+			<div class="pull-right">
+				<ul class="header-top-links">
+					<li><a href="#">Store</a></li>
+					<li><a href="#">Newsletter</a></li>
+					<li><a href="#">FAQ</a></li>
+					<li class="dropdown default-dropdown">
+						<a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">ENG <i class="fa fa-caret-down"></i></a>
+						<ul class="custom-menu">
+							<li><a href="#">English (ENG)</a></li>
+							<li><a href="#">Russian (Ru)</a></li>
+							<li><a href="#">French (FR)</a></li>
+							<li><a href="#">Spanish (Es)</a></li>
+						</ul>
+					</li>
+					<li class="dropdown default-dropdown">
+						<a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">USD <i class="fa fa-caret-down"></i></a>
+						<ul class="custom-menu">
+							<li><a href="#">USD ($)</a></li>
+							<li><a href="#">EUR (€)</a></li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<!-- /top Header -->
+
+	<!-- header -->
+	<div id="header">
+		<div class="container">
+			<div class="pull-left">
+				<!-- Logo -->
+				<div class="header-logo">
+					<a class="logo" href="#">
+						<img src="./img/logo.png" alt="">
+					</a>
+				</div>
+				<!-- /Logo -->
+
+				<!-- Search -->
+				<div class="header-search">
+					<form>
+						<input class="input search-input" type="text" placeholder="Enter your keyword">
+						<select class="input search-categories">
+							<option value="0">All Categories</option>
+							<option value="1">Category 01</option>
+							<option value="1">Category 02</option>
+						</select>
+						<button class="search-btn"><i class="fa fa-search"></i></button>
+					</form>
+				</div>
+				<!-- /Search -->
+			</div>
---
+++ trabalho1/includes/mobile.php
@@ -0,0 +1,13 @@
+<!-- Mobile nav toggle-->
+<li class="nav-toggle">
+    <button class="nav-toggle-btn main-btn icon-btn"><i class="fa fa-bars"></i></button>
+</li>
+<!-- / Mobile nav toggle -->
+</ul>
+</div>
+</div>
+<!-- header -->
+</div>
+<!-- container -->
+</header>
+<!-- /HEADER -->
